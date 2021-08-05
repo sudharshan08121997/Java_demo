b6353700a30ee82b54257802c20e5c987e4ca015
AAMIR HUSSAIN <AAMIR HUSSAIN@DESKTOP-DHH1G6R>
2021-08-05 12:29:21 +0530

    First commit

---
+++ Java_Demo/src/Intro.java
@@ -0,0 +1,15 @@
+class A
+{
+	public void display()
+	{
+		System.out.println("I m Display method from A");
+	}
+}
+public class Intro {
+
+	public static void main(String[] args) {
+		A obj= new A();
+		obj.display();
+	}
+
+}
